eed2bebfb9c04301c417bbe975a9a707ffdbc66f
Jack Coiley <j_coiley@hotmail.co.uk>
2022-01-11 17:09:40 +0000

    Add Colonist Reports mod

---
+++ ColonistReport/ColonistReportsMod.cs
@@ -0,0 +1,23 @@
+﻿using Planetbase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ColonistReport
+{
+    public class ColonistReportsMod : IMod
+    {
+        public void Init()
+        {
+            Debug.Log("[MOD] Colonist Reports activated");
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+}
