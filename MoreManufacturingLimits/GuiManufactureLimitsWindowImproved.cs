164dee3128a06a2084bed36c82e8f7d07b8940b9
Jack Coiley <j_coiley@hotmail.co.uk>
2022-01-20 15:31:07 +0000

    Add MoreManufacturingLimits mod

---
+++ MoreManufacturingLimits/GuiManufactureLimitsWindowImproved.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using UnityEngine;
+using Planetbase;
+
+namespace MoreManufacturingLimits
+{
+	public class GuiManufactureLimitsWindowImproved : GuiWindow
+	{
+		public GuiManufactureLimitsWindowImproved() : base(new GuiLabelItem(StringList.get("manufacture_limits"), TypeList<ModuleType, ModuleTypeList>.find<ModuleTypeFactory>().getIcon(), null, 0, FontSize.Normal), null, null)
+		{
+			this.mHelpId = "manufacture_limits";
+
+			// Manufactured Items
+			var sectionManufacturedResources = new GuiSectionItem(StringList.get("manufacture_limits_manufactured"));
+			var sectionRawResources = new GuiSectionItem(StringList.get("manufacture_limits_raw"));
+
+			foreach (var element in Singleton<ManufactureLimits>.getInstance().mResourceLimits)
+			{
+				var resourceType = element.Key;
+				var limit = element.Value;
+
+				var tooltip = StringList.get("tooltip_manufacture_limit", element.Key.getNamePlural());
+				var guiLabelItem = new GuiLabelItem(resourceType.getNamePlural(), resourceType.getIcon(), tooltip, 0, FontSize.Normal);
+
+				var guiAmountSelector = new GuiAmountSelectorImproved(0, 1000, limit, null, 14);
+				guiAmountSelector.setTooltip(tooltip);
+
+				var guiRowItem = new GuiRowItem(2);
+				guiRowItem.addChild(guiLabelItem);
+				guiRowItem.addChild(guiAmountSelector);
+
+				if (resourceType.hasFlag(64))
+				{
+					sectionManufacturedResources.addChild(guiRowItem);
+				}
+				else
+				{
+					sectionRawResources.addChild(guiRowItem);
+				}
+			}
+
+			// Bots
+			var sectionBots = new GuiSectionItem(StringList.get("manufacture_limits_bots"));
+
+			foreach (Specialization specialization in SpecializationList.getBotSpecializations())
+			{
+				var tooltip = StringList.get("tooltip_manufacture_limit", specialization.getNamePlural());
+				var limit = Singleton<ManufactureLimits>.getInstance().getBotLimit(specialization);
+
+				var guiLabelItem = new GuiLabelItem(specialization.getNamePlural(), specialization.getIcon(), tooltip, 0, FontSize.Normal);
+
+				var guiAmountSelector = new GuiAmountSelectorImproved(0, 1000, limit, null, 14);
+				guiAmountSelector.setTooltip(tooltip);
+
+				GuiRowItem guiRowItem = new GuiRowItem(2);
+				guiRowItem.addChild(guiLabelItem);
+				guiRowItem.addChild(guiAmountSelector);
+
+				sectionBots.addChild(guiRowItem);
+			}
+
+			mRootItem.addChild(sectionManufacturedResources);
+			mRootItem.addChild(sectionRawResources);
+			mRootItem.addChild(sectionBots);
+		}
+
+		public override float getWidth()
+		{
+			return (float)Screen.height * 0.85f;
+		}
+	}
+}
---
+++ MoreManufacturingLimits/MoreManufacturingLimitsMod.cs
@@ -0,0 +1,52 @@
+﻿using ModWrapper;
+using Planetbase;
+using Redirection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace MoreManufacturingLimits
+{
+    public class MoreManufacturingLimitsMod : ModBase
+    {
+        public override void Init()
+        {
+            RegisterStrings();
+
+            Redirector.PerformRedirections();
+
+            TypeList<ResourceType, ResourceTypeList>.find<Metal>().mFlags |= 64;
+
+            Debug.Log("[MOD] MoreManufacturingLimits activated");
+        } 
+
+        private static void RegisterStrings()
+        {
+            StringList.mStrings.Add("manufacture_limits_manufactured", "Manufactured");
+            StringList.mStrings.Add("manufacture_limits_raw", "Resources");
+            StringList.mStrings.Add("manufacture_limits_bots", "Bots");
+        }
+
+        public override void Update(float timeStep)
+        {
+
+        }
+
+        public override void OnGameStart()
+        {
+            if (MenuSystem != null && MenuSystem.mMenuBaseManagement is GuiMenu menuBaseManagement)
+            {
+                foreach(var menuItem in menuBaseManagement.mItems)
+                {
+                    if(menuItem.getRequiredModuleType() == TypeList<ModuleType, ModuleTypeList>.find<ModuleTypeFactory>())
+                    {
+                        menuItem.mCallback = new GuiDefinitions.Callback(Game.toggleWindow<GuiManufactureLimitsWindowImproved>);
+                    }
+                }
+            }
+        }
+    }
+}
