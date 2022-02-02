6ef537be1e32d26346dc999272ababa67c46897e
Jack Coiley <j_coiley@hotmail.co.uk>
2022-02-02 12:42:15 +0000

    Create QoLTweaks Mod

---
+++ QoLTweaks/QoLTweaksMod.cs
@@ -0,0 +1,46 @@
+﻿using ModWrapper;
+using Planetbase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace QoLTweaks
+{
+    public class QoLTweaksMod : ModBase, IMod
+    {
+        const int NewMedicalCabinetCapacity = 12;
+
+        public override void Init()
+        {
+            Debug.Log("[MOD] QoL Tweaks activated");
+        }
+
+        public override void OnGameStart()
+        {
+            SetMedicalCabinetCapacity();
+        }
+
+        private void SetMedicalCabinetCapacity()
+        {
+            // Update ResourceType definition
+            TypeList<ComponentType, ComponentTypeList>.find<MedicalCabinet>().mEmbeddedResourceCount = NewMedicalCabinetCapacity;
+
+            // Update all existing cabinets
+            foreach (var component in ConstructionComponent.mComponents)
+            {
+                if (component.getComponentType() is MedicalCabinet cabinet)
+                {
+                    component.mResourceContainer.setCapacity(NewMedicalCabinetCapacity);
+                }
+            }
+        }
+
+        public override void Update(float timeStep)
+        {
+            // Nothing required here
+        }
+    }
+}
