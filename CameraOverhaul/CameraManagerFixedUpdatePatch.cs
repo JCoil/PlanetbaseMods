b8559700d0a183c9706725f80f0811a5571eadb1
Jack Coiley <j_coiley@hotmail.co.uk>
2022-02-23 14:07:15 +0000

    Redirect all member access through reflection utils Refactor into seperate classes for Harmony Patcher

---
+++ CameraOverhaul/CameraManagerFixedUpdatePatch.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Planetbase;
+using PlanetbaseModUtilities;
+using static UnityModManagerNet.UnityModManager;
+using UnityEngine;
+using HarmonyLib;
+
+namespace CameraOverhaul
+{
+    [HarmonyPatch(typeof(CameraManager), "fixedUpdate")]
+    public static class CameraManagerFixedUpdatePatch
+    {
+        public static bool Prefix(ref CameraManager __instance, float timeStep, int frameIndex)
+        {
+            var cameraManager = __instance;
+
+            if (!cameraManager.isCinematic())
+            {
+                float lateralMoveSpeed = timeStep * 6f;
+                float zoomAndRotationSpeed = timeStep * 10f;
+
+                GameState gameState = GameManager.getInstance().getGameState();
+
+                // this only happens when placing a module and only if current height < 21
+                var currentHeight = cameraManager.GetCurrentHeight();
+                var targetHeight = cameraManager.GetTargetHeight();
+
+                if (targetHeight != currentHeight)
+                {
+                    cameraManager.SetCurrentHeight(currentHeight + Mathf.Sign(targetHeight - currentHeight) * timeStep * 30f);
+
+                    if (Mathf.Abs(currentHeight - targetHeight) < 0.5f)
+                    {
+                        cameraManager.SetCurrentHeight(targetHeight);
+                    }
+                }
+
+                if (gameState is GameStateGame game && !game.isCameraFixed() && !TimeManager.getInstance().isPaused())
+                {
+                    KeyBindingManager keyBindingManager = KeyBindingManager.getInstance();
+
+                    if (game != null && game.GetModeValue() == CameraOverhaul.ModePlacingModule)
+                    {
+                        if (!CameraOverhaul.mIsPlacingModule)
+                        {
+                            CameraOverhaul.mIsPlacingModule = true;
+                            CameraOverhaul.mModulesize = game.GetCurrentModuleSize();
+                        }
+
+                        var mZoomAxis = cameraManager.GetZoomAxis();
+
+                        // we're zooming
+                        if (Mathf.Abs(mZoomAxis) > 0.001f || Mathf.Abs(keyBindingManager.getCompositeAxis(ActionType.CameraZoomOut, ActionType.CameraZoomIn)) > 0.001f)
+                        {
+                            if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
+                            {
+                                if (mZoomAxis <= -0.1f || keyBindingManager.getBinding(ActionType.CameraZoomOut).justUp())
+                                {
+                                    if (CameraOverhaul.mModulesize > game.GetPlacedModuleType().getMinSize())
+                                    {
+                                        CameraOverhaul.mModulesize--;
+                                    }
+                                }
+                                else if ((mZoomAxis >= 0.1f || keyBindingManager.getBinding(ActionType.CameraZoomIn).justUp()) && CameraOverhaul.mModulesize < game.GetPlacedModuleType().getMaxSize())
+                                {
+                                    CameraOverhaul.mModulesize++;
+                                }
+                            }
+                        }
+
+                        game.SetCurrentModuleSize(CameraOverhaul.mModulesize);
+                    }
+                    else
+                    {
+                        CameraOverhaul.mIsPlacingModule = false;
+                    }
+
+                    var mAcceleration = cameraManager.GetAcceleration();
+
+                    mAcceleration.x += keyBindingManager.getCompositeAxis(ActionType.CameraMoveLeft, ActionType.CameraMoveRight) * lateralMoveSpeed;
+                    mAcceleration.z += keyBindingManager.getCompositeAxis(ActionType.CameraMoveBack, ActionType.CameraMoveForward) * lateralMoveSpeed;
+
+                    if (!Input.GetKey(KeyCode.LeftControl) && !Input.GetKey(KeyCode.RightControl))
+                    {
+                        mAcceleration.y -= cameraManager.GetZoomAxis() * zoomAndRotationSpeed;
+                        mAcceleration.y -= keyBindingManager.getCompositeAxis(ActionType.CameraZoomOut, ActionType.CameraZoomIn) * zoomAndRotationSpeed;
+                    }
+
+                    CameraOverhaul.mAlternateRotationAcceleration -= keyBindingManager.getCompositeAxis(ActionType.CameraRotateLeft, ActionType.CameraRotateRight) * zoomAndRotationSpeed;
+
+                    // Rotate with middle mouse button
+                    if (Input.GetMouseButton(2))
+                    {
+                        float mouseDeltaX = Input.mousePosition.x - cameraManager.GetPreviousMouseX();
+                        if (Mathf.Abs(mouseDeltaX) > Mathf.Epsilon)
+                        {
+                            cameraManager.SetRotationAcceleration(cameraManager.GetRotationAcceleration() + zoomAndRotationSpeed * mouseDeltaX * 0.1f);
+                        }
+
+                        float mouseDeltaY = Input.mousePosition.y - CameraOverhaul.mPreviousMouseY;
+                        if (Mathf.Abs(mouseDeltaY) > Mathf.Epsilon)
+                        {
+                            CameraOverhaul.mVerticalRotationAcceleration += zoomAndRotationSpeed * mouseDeltaY * 0.1f;
+                        }
+                    }
+
+                    // Move with mouse on screen borders
+                    if (!Application.isEditor)
+                    {
+                        float screenBorder = Screen.height * 0.01f;
+                        if (Input.mousePosition.x < screenBorder)
+                        {
+                            mAcceleration.x = mAcceleration.x - lateralMoveSpeed;
+                        }
+                        else if (Input.mousePosition.x > Screen.width - screenBorder)
+                        {
+                            mAcceleration.x = mAcceleration.x + lateralMoveSpeed;
+                        }
+                        if (Input.mousePosition.y < screenBorder)
+                        {
+                            mAcceleration.z = mAcceleration.z - lateralMoveSpeed;
+                        }
+                        else if (Input.mousePosition.y > Screen.height - screenBorder)
+                        {
+                            mAcceleration.z = mAcceleration.z + lateralMoveSpeed;
+                        }
+                    }
+
+                    float clampSpeed = !Input.GetKey(KeyCode.LeftShift) ? 1f : 0.25f;
+                    mAcceleration.x = Mathf.Clamp(mAcceleration.x - mAcceleration.x * lateralMoveSpeed, -clampSpeed, clampSpeed);
+                    mAcceleration.z = Mathf.Clamp(mAcceleration.z - mAcceleration.z * lateralMoveSpeed, -clampSpeed, clampSpeed);
+                    mAcceleration.y = Mathf.Clamp(mAcceleration.y - mAcceleration.y * zoomAndRotationSpeed, -clampSpeed, clampSpeed);
+
+                    cameraManager.SetAcceleration(mAcceleration);
+                    cameraManager.SetRotationAcceleration(Mathf.Clamp(cameraManager.GetRotationAcceleration() - cameraManager.GetRotationAcceleration() * zoomAndRotationSpeed, -clampSpeed, clampSpeed));
+
+                    CameraOverhaul.mVerticalRotationAcceleration = Mathf.Clamp(CameraOverhaul.mVerticalRotationAcceleration - CameraOverhaul.mVerticalRotationAcceleration * zoomAndRotationSpeed, -clampSpeed, clampSpeed);
+                    CameraOverhaul.mAlternateRotationAcceleration = Mathf.Clamp(CameraOverhaul.mAlternateRotationAcceleration - CameraOverhaul.mAlternateRotationAcceleration * zoomAndRotationSpeed, -clampSpeed, clampSpeed);
+                }
+                else
+                {
+                    cameraManager.SetAcceleration(Vector3.zero);
+                    cameraManager.SetRotationAcceleration(0f);
+                    CameraOverhaul.mVerticalRotationAcceleration = 0f;
+                    CameraOverhaul.mAlternateRotationAcceleration = 0f;
+                }
+
+                cameraManager.SetPreviousMouseX(Input.mousePosition.x);
+                CameraOverhaul.mPreviousMouseY = Input.mousePosition.y;
+            }
+            else
+            {
+                cameraManager.getCinematic().fixedUpdate(timeStep);
+            }
+
+            cameraManager.SetZoomAxis(0f);
+
+            return false; // Want to skip original code
+        }
+    }
+}
---
+++ CameraOverhaul/CameraManagerUpdatePatch.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Planetbase;
+using PlanetbaseModUtilities;
+using static UnityModManagerNet.UnityModManager;
+using UnityEngine;
+using HarmonyLib;
+
+namespace CameraOverhaul
+{
+    [HarmonyPatch(typeof(CameraManager), "update")]
+    public abstract class CameraManagerUpdatePatch
+    {
+        public static bool Prefix(ref CameraManager __instance, float timeStep)
+        {
+            var cameraManager = __instance;
+
+
+            if (cameraManager.GetZoomAxis() != 0f)
+            {
+                cameraManager.SetZoomAxis(Input.GetAxis("Zoom"));
+            }
+
+            if (GameManager.getInstance().getGameState() is GameStateGame game && !game.isCameraFixed())
+            {
+                if (!cameraManager.isCinematic())
+                {
+                    if (game.GetModeValue() == CameraOverhaul.ModePlacingModule && CameraOverhaul.mIsPlacingModule)
+                    {
+                        game.SetCurrentModuleSize(CameraOverhaul.mModulesize);
+                    }
+
+                    Transform transform = cameraManager.getCamera().transform;
+                    var acceleration = cameraManager.GetAcceleration();
+
+                    float xAxis = acceleration.x;
+                    float yAxis = acceleration.y;
+                    float zAxis = acceleration.z;
+                    float absXAxis = Mathf.Abs(xAxis);
+                    float absYAxis = Mathf.Abs(yAxis);
+                    float absZAxis = Mathf.Abs(zAxis);
+
+                    if (!cameraManager.IsLocked())
+                    {
+                        // if zooming
+                        if (absYAxis > 0.01f)
+                        {
+                            float speed = Mathf.Clamp(60f * timeStep, 0.01f, 100f);
+                            float newHeight = Mathf.Clamp(cameraManager.GetCurrentHeight() + yAxis * speed,CameraOverhaul.MIN_HEIGHT, CameraOverhaul.MAX_HEIGHT);
+
+                            if (transform.eulerAngles.x < 86f)
+                            {
+                                zAxis += (cameraManager.GetCurrentHeight() - newHeight) / speed;
+                                absZAxis = Mathf.Abs(zAxis);
+                            }
+
+                            cameraManager.SetCurrentHeight(newHeight);
+                            cameraManager.SetTargetHeight(newHeight);
+                        }
+
+                        // Move forwards
+                        if (absZAxis > 0.001f)
+                        {
+                            transform.position += new Vector3(transform.forward.x, 0f, transform.forward.z).normalized * zAxis * timeStep * 80f;
+                        }
+
+                        // Move sideways
+                        if (absXAxis > 0.001f)
+                        {
+                            transform.position += new Vector3(transform.right.x, 0f, transform.right.z).normalized * xAxis * timeStep * 80f;
+                        }
+
+                        // rotate around cam
+                        Vector3 eulerAngles = transform.eulerAngles;
+                        if (Mathf.Abs(cameraManager.GetRotationAcceleration()) > 0.01f)
+                        {
+                            eulerAngles.y += cameraManager.GetRotationAcceleration() * timeStep * 120f;
+                        }
+
+                        if (Mathf.Abs(CameraOverhaul.mVerticalRotationAcceleration) > 0.01f)
+                        {
+                            eulerAngles.x = Mathf.Clamp(eulerAngles.x - CameraOverhaul.mVerticalRotationAcceleration * timeStep * 120f, 20f, 87f);
+                        }
+
+                        transform.eulerAngles = eulerAngles;
+                    }
+                    else if (absYAxis > 0.01f)
+                    {
+                        float speed = Mathf.Clamp(60f * timeStep, 0.01f, 100f);
+                        Vector3 movement = transform.forward * speed * -yAxis;
+
+                        Vector3 planePoint = Selection.getSelectedConstruction().getPosition();
+                        planePoint.y = yAxis < 0f ? 4f : Selection.getSelectedConstruction().getRadius() + 10f;
+                        Plane plane = new Plane(Vector3.up, planePoint);
+
+                        Ray ray = new Ray(transform.position, yAxis < 0f ? transform.forward : -transform.forward);
+
+                        if (plane.Raycast(ray, out float dist))
+                        {
+                            if (dist < movement.magnitude)
+                            {
+                                movement *= dist / movement.magnitude;
+                            }
+
+                            transform.position += movement;
+                        }
+                    }
+
+                    // rotate around world
+                    if (Mathf.Abs(CameraOverhaul.mAlternateRotationAcceleration) > 0.01f)
+                    {
+                        Ray ray = new Ray(transform.position, transform.forward);
+                        float dist;
+                        if (CameraOverhaul.mGroundPlane.Raycast(ray, out dist))
+                        {
+                            transform.RotateAround(transform.position + transform.forward * dist, Vector3.up, CameraOverhaul.mAlternateRotationAcceleration * timeStep * 120f);
+                        }
+                    }
+
+                    // if we moved, set the correct height
+                    if (!cameraManager.IsLocked() && (absZAxis > 0.001f || absXAxis > 0.001f || absYAxis > 0.01f))
+                    {
+                        cameraManager.placeOnFloor(cameraManager.GetCurrentHeight());
+                    }
+
+                    // Calc map center and distance
+                    Vector3 mapCenter = new Vector3(CameraOverhaul.TerrainTotalSize, 0f, CameraOverhaul.TerrainTotalSize) * 0.5f;
+                    Vector3 mapCenterToCam = transform.position - mapCenter;
+                    float distToMapCenter = mapCenterToCam.magnitude;
+
+                    // limit cam to 375 units from center
+                    if (distToMapCenter > 375f)
+                    {
+                        cameraManager.getCamera().transform.position = mapCenter + mapCenterToCam.normalized * 375f;
+                    }
+                }
+                else
+                {
+                    cameraManager.updateCinematic(timeStep);
+                }
+            }
+
+            // interpolate the position when the game moves the camera to a specific location (e.g when editing a building)
+            if (cameraManager.isTransitioning())
+            {
+                if (cameraManager.GetTransitionTime() == 0f)
+                {
+                    cameraManager.SetCameraTransition(1f);
+                }
+                else
+                {
+                    float num4 = timeStep / cameraManager.GetTransitionTime();
+                    cameraManager.SetCameraTransition(cameraManager.GetCameraTransition() + num4);
+                }
+                cameraManager.GetCurrentTransform().interpolate(
+                    cameraManager.GetSourceTransform(),
+                    cameraManager.GetTargetTransform(),
+                    cameraManager.GetCameraTransition());
+
+                cameraManager.GetCurrentTransform().apply(cameraManager.getCamera().transform);
+            }
+
+            cameraManager.GetSkydomeCamera().transform.rotation = cameraManager.getCamera().transform.rotation;
+
+            return false; // Want to skip original code
+        }
+    }
+}
