b8bcd9c12ccea7c01da683cfa1aae9981f53708d
Jack Coiley <j_coiley@hotmail.co.uk>
2024-02-01 17:07:14 +0000

    Add connections code

--- CheatTools/Patches/ModulePatches.cs
+++ CheatTools/Patches/ModulePatches.cs
@@ -3,11 +3,13 @@ using Planetbase;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
+using UnityEngine;
 
 namespace CheatTools.Patches
 {
-    [HarmonyPatch(typeof(Module), "canPlaceModule")]
+    [HarmonyPatch(typeof(Module))]
     public class ModulePatches
     {
         [HarmonyPostfix]
@@ -20,4 +22,23 @@ namespace CheatTools.Patches
             }
         }
     }
+
+    //[HarmonyPatch(typeof(Connection))]
+    //public class ConnectionPatches
+    //{
+    //    [HarmonyTranspiler]
+    //    [HarmonyPatch(argumentTypes: new Type[] { typeof(Module), typeof(Module), typeof(Vector3), typeof(Vector3) }, methodName: nameof(Connection.canLink))]
+    //    static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    //    {
+    //        var code = new List<CodeInstruction>(instructions);
+
+    //        // We'll need to modify code here.
+    //        if (code.FirstOrDefault(x => x.opcode == OpCodes.Ldc_R4 && (float)x.operand == 20f) is CodeInstruction instruction)
+    //        {
+    //            instruction.operand = 100f;
+    //        }
+
+    //        return code;
+    //    }
+    //}
 }
