62067ef08c9a819e9f6f25991340a15f780cf982
Jack Coiley <j_coiley@hotmail.co.uk>
2024-02-15 17:48:48 +0000

    Initial mod setup

---
+++ PowerMonitor/PowerMonitor.cs
@@ -0,0 +1,30 @@
+﻿using Planetbase;
+using PlanetbaseModUtilities;
+using static UnityModManagerNet.UnityModManager;
+
+namespace PowerMonitor
+{
+    public class PowerMonitor : ModBase
+    {
+        public static new void Init(ModEntry modEntry) => InitializeMod(new PowerMonitor(), modEntry, "PowerMonitor");
+
+        public override void OnInitialized(ModEntry modEntry)
+        {
+            RegisterStrings();
+        }
+
+        private static void RegisterStrings()
+        {
+            //StringUtils.RegisterString("fill_with_bunks", "Fill With Bunks");
+        }
+
+        public override void OnGameStart(GameStateGame gameStateGame)
+        {
+        }
+
+        public override void OnUpdate(ModEntry modEntry, float timeStep)
+        {
+            // Nothing required here for now
+        }
+    }
+}
