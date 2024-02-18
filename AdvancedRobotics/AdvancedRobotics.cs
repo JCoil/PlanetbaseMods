e35e9880410ea54e8a29a94699b21ef9c339dbd5
Jack Coiley <j_coiley@hotmail.co.uk>
2024-02-18 14:43:54 +0000

    Initial setup for new mod

---
+++ AdvancedRobotics/AdvancedRobotics.cs
@@ -0,0 +1,30 @@
+﻿using Planetbase;
+using PlanetbaseModUtilities;
+using static UnityModManagerNet.UnityModManager;
+
+namespace AdvancedRobotics
+{
+    public class AdvancedRobotics : ModBase
+    {
+        public static new void Init(ModEntry modEntry) => InitializeMod(new AdvancedRobotics(), modEntry, "AdvancedRobotics");
+
+        public override void OnInitialized(ModEntry modEntry)
+        {
+            RegisterStrings();
+        }
+
+        private static void RegisterStrings()
+        {
+        }
+
+        public override void OnGameStart(GameStateGame gameStateGame)
+        {
+        }
+
+        public override void OnUpdate(ModEntry modEntry, float timeStep)
+        {
+            // Nothing required here for now
+        }
+    }
+}
+
